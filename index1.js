e79c35a0fa1399a9712c18d203e683bb6d8afbb3
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-06-09 16:13:43 -0400

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/payment", function (req, res) {
+  res.sendFile(path.join(__dirname, "payment.html"));
+});
+
+app.get("/products", function (req, res) {
+  res.sendFile(path.join(__dirname, "Products.html"));
+});
+
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
