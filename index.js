8e0a12597463533b2423a7b70b993d1b0bf8565f
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-06-24 17:35:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ MyMenu += '  <div class="navbar">';
 MyMenu += '    <nav class="navbar navbar-inverse">';
 MyMenu += ' 		<div class="container">';
 MyMenu += ' 			<ul class="nav navbar-nav navbar-right">';
-MyMenu += '					<li><a class="glyphicon glyphicon-home" href="/"> Home</a></li>';
-MyMenu += '					<li><a class="glyphicon glyphicon-shopping-cart" href="products"> Products</a></li>';
-MyMenu += '					<li><a class="glyphicon glyphicon-search" href="About.html"> About</a></li>';
-MyMenu += '					<li><a class="glyphicon glyphicon-envelope" href="#"> Contact</a></li>';
-MyMenu += '					<li><a class="glyphicon glyphicon-user" href="#"> Account</a></li>';
+MyMenu += '					<li><a class="glyphicon glyphicon-home" href="index.html"> Home</a></li>';
+MyMenu += '					<li><a class="glyphicon glyphicon-shopping-cart" href="Products.html"> Products</a></li>';
 MyMenu += '				</ul>';
 MyMenu += '			</div>';
 MyMenu += '		</nav>';
@@ -17,3 +14,4 @@ MyMenu += '		</nav>';
 
   document.getElementById('navigator').innerHTML = MyMenu;
 }
+
