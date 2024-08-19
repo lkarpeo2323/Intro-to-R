bf97ad8f7934edb3c40dc6cf4234f6456270cfba
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-19 10:12:46 -0400

    Create Average MLB Runs by Year.R

---
+++ Average MLB Runs by Year.R
@@ -0,0 +1,23 @@
+install.package("Lahman")
+
+library(Lahman)
+library(dplyr)
+library(ggplot2)
+library(caret)
+
+teams <- Teams
+
+teams <- teams %>%
+  mutate(runs_game = R / (W + L))
+
+teams_year <- teams %>%
+  group_by(yearID) %>%
+  summarize(mean_runs = mean(runs_game, na.rm=TRUE))
+
+teams_year %>%
+  ggplot(aes(x=yearID,y=mean_runs)) +
+  geom_line()+
+  geom_point()+
+  ggtitle('Average MLB Runs by Year')
+head(teams_year)
+
