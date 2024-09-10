4bdb8cd8dec0b78b93424cdf4b7a511c51cd1bc4
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-10 15:41:46 -0400

    Update Notes Today.R

--- codes/Notes Today.R
+++ codes/Notes Today.R
@@ -4,7 +4,6 @@ c = -10
 
 (a*sqrt(b))/(1-(1+a)^c)
 
-
 a =.6
 b = 2000
 c = -10
@@ -15,3 +14,18 @@ factor  = function(a,b,c){
   }
 
 factor(.06,2000,-10)
+
+
+
+#lm function 
+a = 1:10
+b = 11:20
+
+reg = lm(b~a)
+
+reg
+
+#Correlation Test
+corr.test(a,b)
+
+
