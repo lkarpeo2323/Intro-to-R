d322d15cc661ba3f0666b90f3264dd9cd84cec5f
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-12 15:08:01 -0400

    Update Import and Export.R

--- Week 2/Import and Export.R
+++ Week 2/Import and Export.R
@@ -1,7 +1,18 @@
 name = c("Jon","Bill", "Maria","Ben","Tina")
-
 age  = c(23,41,32,58,26)
-
 staff = data.frame(name,age)
-
 staff
+
+
+#Exercise 1
+
+Student = c("Apple","Ben","Carol","Debby","Eddie")
+Grade = c(80,75,92,86,78)
+Class = data.frame(Student,Grade)
+Class
+
+
+
+
+
+
