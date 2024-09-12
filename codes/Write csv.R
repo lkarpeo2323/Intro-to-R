787501cbffc8ce370759aca1e919503e058653f2
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-12 15:57:50 -0400

    Update Write csv.R

--- codes/Write csv.R
+++ codes/Write csv.R
@@ -4,3 +4,8 @@ name = c("Jon","Bill", "Maria","Ben","Tina")
 age  = c(23,41,32,58,26)
 Staff = data.frame(name,age)
 write.csv(Staff,"output.csv")
+
+#Write and read csv and text files
+text_file = read.table("Amazon.txt",header=TRUE)
+csv_file = read.csv("Amazon.csv",header=TRUE)
+print(csv_file)
