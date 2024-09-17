05d5da3572075456724001a5bab7d022946291f2
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-17 15:14:30 -0400

    Update Sept 17 Lecture.R

--- Week 2/Sept 17 Lecture.R
+++ Week 2/Sept 17 Lecture.R
@@ -1,1 +1,8 @@
-khkhkhk
+#Exercise 2
+
+Student = c("Apple","Ben","Carol","Debby","Eddie")
+Grade = c(80,75,92,86,78)
+Class = data.frame(Student,Grade)
+Class
+
+write.csv(Class,"output.csv",row.names=FALSE)
