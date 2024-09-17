5aa23ff25198f0327776eb1ccac71d11528908b6
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-17 15:34:04 -0400

    Update Sept 17 Lecture.R

--- Week 2/Sept 17 Lecture.R
+++ Week 2/Sept 17 Lecture.R
@@ -11,10 +11,22 @@ write.csv(Class,"output.csv",row.names=FALSE)
 
 #Exersise 3
 
+
 data = read.csv('Professional.csv')
 names(data)
-class(data)
+class(data$Age)
+class(data$Gender)
+class(data$Household.Income)
 str(data)
 head(data, n=5)
 tail(data, n=5)
-mean(data$age)
+mean(data$Age)
+mean(data$Household.Income)
+median(data$Age)
+median(data$Household.Income)
+min(data$Age)
+min(data$Household.Income)
+max(data$Age)
+max(data$Household.Income)
+
+
