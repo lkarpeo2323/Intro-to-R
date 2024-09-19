670ebffe91d421e08e13575c8be7c8912381a1b9
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-19 15:44:05 -0400

    Update Sept 19 Lecture.R

--- Week 2/Sept 19 Lecture.R
+++ Week 2/Sept 19 Lecture.R
@@ -1,3 +1,16 @@
 #Exercise 2
 
 seq(0,1,.1) # From 0 to 1 adding by 0.1
+
+
+#Exercise 3 
+
+var.1 = c(65,30,75,65,65,30,60,30,65,45)
+var.2 = c(1,2,3,4,5,6,7,8,9,10)
+
+division = var.1/var.2
+division
+
+
+#Exercise 4
+
