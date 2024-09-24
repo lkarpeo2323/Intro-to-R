29b24747dba2e6ec1b5e73ac8df56ae4d3281fa1
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-24 15:40:32 -0400

    Update Sept 24 Lecture.R

--- Week 2/Sept 24 Lecture.R
+++ Week 2/Sept 24 Lecture.R
@@ -2,9 +2,10 @@
 rep(1:10,5)
 
 #Exercise 7
-
 var.1 = c(65,30,75,65,65,30,60,30,65,45)
-var.2 = c(1,2,3,4,5,6,7,8,9,10)
-
-unique
-
+unique_values = unique(var.1)
+sorted = sort(unique_values)
+lowest = sorted[1:3]
+lowest
+z = lowest[c(1,3)]
+z
