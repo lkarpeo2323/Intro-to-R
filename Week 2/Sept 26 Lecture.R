66e156c820b17079ff736b8d23a834faae8b040d
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-09-26 14:52:17 -0400

    Update Sept 26 Lecture.R

--- Week 2/Sept 26 Lecture.R
+++ Week 2/Sept 26 Lecture.R
@@ -1,7 +1,7 @@
 #Exercise 9
 
-Student = c("Abby", "Bob", "Cindy", "Cindy","Debby","Ed")
+Student = c("Abby", "Bob", "Cindy","Debby","Ed")
 Score = c(78,86,94,85,76)
 
-
 data = data.frame(Student,Score)
+data
