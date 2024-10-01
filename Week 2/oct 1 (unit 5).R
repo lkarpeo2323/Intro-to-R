3d61c453e937c185ebba9a4d77600d07f1f8b003
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-01 15:44:47 -0400

    Update oct 1 (unit 5).R

--- Week 2/oct 1 (unit 5).R
+++ Week 2/oct 1 (unit 5).R
@@ -1,1 +1,15 @@
+#Exercise 3
+
+a = seq(1:10)
+b = seq(11,29,2)
+c = seq(12,30,2)
+income = cbind(a,b,c)
+colnames(income)=c("A","B","C")
+
+
+income.2 <- income * 1000  # Multiply each element by 1000
+
+income.3 = income.2
+income.3[c(6,7),1] = income.2[c(6,7),1]/5
+income.3
 
