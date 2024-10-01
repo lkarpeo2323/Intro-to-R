44e085ec292eda2dbf6a883b996ab5f431bb4def
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-01 15:58:20 -0400

    Update oct 1 (unit 5).R

--- Week 2/oct 1 (unit 5).R
+++ Week 2/oct 1 (unit 5).R
@@ -7,10 +7,9 @@ income = cbind(a,b,c)
 colnames(income)=c("A","B","C")
 income.2 <- income * 1000  # Multiply each element by 1000
 income.3 = income.2
-income.3[c(6,7),1] = income.2[c(6,7),1]/5
+income.3[c(6,7),1] = income.2[c(6,7),1]/5 #Divide col 1 row 6 and 7 by 2
 income.3[1, "C"] = 6000
-income.3[2, "B"] = income.3[2, "B"] / 4
-income.3[8:10, "B"] = income.3[8:10, "B"] / 4
+income.3[c(2,8:10), "B"] = income.3[c(2,8:10), "B"] / 4 #Divide col "B" row 2, and 8 through 10  by 4
 income.3[2:10, "C"] = income.3[2:10, "C"] / 2
 income.3[income.3 > 15000 & income.3 < 20000] = 18000
 income.4 = income.3[, c("A", "B")]
