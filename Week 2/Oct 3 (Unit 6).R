f65c115027df6914d99a0e925511b64f02fb22f2
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-06 14:49:46 -0400

    Update Oct 3 (Unit 6).R

--- Week 2/Oct 3 (Unit 6).R
+++ Week 2/Oct 3 (Unit 6).R
@@ -4,3 +4,11 @@ v = 1:12
 rn = c("id_1","id_2")
 cn = c("v.1","v.2","v.3")
 sn = c("store.1","store.2")
+
+#Approach 1
+a = array(v, dim=c(2,3,2),dimnames=list(rn,cn,sn))
+a
+
+#Approach 2
+dim(v) = c(2,3,2)
+v
