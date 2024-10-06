5585d3c929598b7feb7f3525e1254065f7deafc8
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-06 17:36:04 -0400

    Update HW3.R

--- HW/HW3.R
+++ HW/HW3.R
@@ -20,30 +20,37 @@ str(gap2)
 #2E: Variable Names
 
 colnames(gap2) #"breastcancer"     "femaleemployrate" "employrate" 
+gap2
 
 #3A: Create the Matrix
 
 m.1 = matrix(1:346,nrow=173,ncol = 2,byrow = TRUE)
 colnames(m.1)=c("Odd","Even")
+m.1
 
 #3B
 m.1[m.1>100]=100
 m.1/100
-
+m.1
 
 #3C: Combine gap2 and m.1 into a new matrix
 gap3 = cbind(gap2,m.1)
-
+gap3
 
 #4
 
 
 breastcancer2 = as.numeric(gap3[, "breastcancer"]) * as.numeric(gap3[, "Odd"])
 gap3 = cbind(gap3, breastcancer2)
-
+gap3
 
 #5: Averages and Totals
 
 totals = colSums(gap3, na.rm = TRUE)
+averages = colMeans(gap3, na.rm = TRUE)
+
+totals
+averages
+
 
 
