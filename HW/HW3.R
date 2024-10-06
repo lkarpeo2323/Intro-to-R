f61ae57afa45f433a0f8689c326cc2d14a0b1b2a
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-06 16:02:45 -0400

    Update HW3.R

--- HW/HW3.R
+++ HW/HW3.R
@@ -5,3 +5,12 @@ gap = read.csv('gap.csv', header=TRUE)
 #2A
 
 gap2= gap[2:4]
+
+#2B: structure of the data
+
+str(gap2) #173 obs. of  3 variables:
+
+#2C: As a matrix 
+
+gap2 = as.matrix(gap2)
+gap2
