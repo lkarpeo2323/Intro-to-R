4649ffa2c22296393e2a8ced0fedcbe82f976608
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-10 15:27:37 -0400

    Update and rename Oct 10 (Unit 5).R to Oct 10 (Unit 6 and 7).R

---
+++ Week 2/Oct 10 (Unit 6 and 7).R
@@ -0,0 +1,15 @@
+#Exercise 3
+
+rowSums(b[,,"store.1"])
+
+colSums(b[,,"store.1"])
+
+sum(b[,,"store.1"])
+
+rowSums(b[,,"store.2"])
+
+colSums(b[,,"store.2"])
+
+sum(b[,,"store.2"])
+
+rowSums(colSums(b))
