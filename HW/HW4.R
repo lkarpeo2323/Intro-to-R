c8a86eda2d3f2f7a2e097d7a574fd63babf168ae
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-26 16:12:13 -0400

    Update HW4.R

--- HW/HW4.R
+++ HW/HW4.R
@@ -1,2 +1,20 @@
-#1
+#1a
 
+sales = read.csv("sales.csv")
+
+#1b
+str(sales)
+
+#1c
+
+sales1 = as.matrix(sales)
+sales1
+
+#1D
+
+colnames(sales1) #"UnitSold" "Revenue"  "Cost"
+ncol(sales1) #3 columns
+nrow(sales1) #12 rows
+
+sales2 = 2*sales1
+sales2
