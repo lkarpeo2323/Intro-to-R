79a0a4b52c4f0e93c1e38a9a2e0d6f6425a448b2
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-29 15:21:55 -0400

    Update Oct 29 (Unit 8).R

--- Post Midterm/Oct 29 (Unit 8).R
+++ Post Midterm/Oct 29 (Unit 8).R
@@ -6,4 +6,13 @@ program = c(F,T,T,F,F)
 
 
 students = data.frame(name,grade,program)
+
+standing = c("senior","junior","junior","senior","senior")
+credits = c(9,12,9,9,6)
+commute = c(T,T,F,T,F)
+
+students = cbind(students,standing,credits,commute)
 students
+summary(students)
+
+
