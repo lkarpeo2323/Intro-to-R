def10c135758a676d8c4133d368d0ab8ae3229f9
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-29 15:13:52 -0400

    Update Oct 29 (Unit 8).R

--- Post Midterm/Oct 29 (Unit 8).R
+++ Post Midterm/Oct 29 (Unit 8).R
@@ -1,9 +1,9 @@
-name <- c("Mickey","Minnie","Tong","Yoyo")
-age <- 26:29
-female <- c(F,T,T,F)
-seniority <- c(3, 5, 4, 10)
-mydata <- data.frame(name, age, female, seniority)
-mydata
+#Exercise 3
 
-mydata[["age"]][1] #Retrieve the first "Age" value
-mydata[mydata$female==T,] #Retrieve Data where "Female" is True
+name = c("Amy","Bob","Cindy","David","Eddie")
+grade = c(88,98,92,82,85)
+program = c(F,T,T,F,F)
+
+
+students = data.frame(name,grade,program)
+students
