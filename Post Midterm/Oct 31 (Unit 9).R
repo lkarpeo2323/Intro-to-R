0b65ee9c9990f15c0169f95f6c1e9c3864bc9baa
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-10-31 15:05:33 -0400

    Update Oct 31 (Unit 9).R

--- Post Midterm/Oct 31 (Unit 9).R
+++ Post Midterm/Oct 31 (Unit 9).R
@@ -1,1 +1,26 @@
+#Exercise 1
+
+name <- c("Amy","Bob","Cindy","David","Eddie","Fanny","Greg","Harry")
+grade <- c(88,98,92,82,85,65,70,67)
+standing <- c("senior","junior","sophomore","senior","senior","sophomore","freshmen","junior")
+students <- data.frame(name,grade,standing)
+
+# Check if 'standing' is a factor
+is.factor(students$standing)
+
+# Convert 'standing' to a factor
+students$standing.f = factor(students$standing)
+
+#set the levels (#The first one is the greatest)
+students$standing.f2 = factor(students$standing,levels = c("freshmen","sophomore","junior", "senior"))
+
+#check if its a factor
+is.factor(students$standing.f2)
+
+#Get the Frequency
+table(students$standing.f2)  
+
+
+
+
 
