f4c9a513bab9e571e691176d323cd306b0b0fe20
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-05 14:56:43 -0500

    Update Nov 5 (Unit 10).R

--- Post Midterm/Nov 5 (Unit 10).R
+++ Post Midterm/Nov 5 (Unit 10).R
@@ -1,9 +1,18 @@
 #Exercise 2
-
 x = 40
-
 if (x>=60){
-  print("pass")
-  }else{
+  print("Pass")
+}else{
   print("Fail")
+}
+#Self-Defined Function
+
+leo = function(x){
+  if (x>=60){
+    print("Pass")
+  }else{
+    print("Fail")
   }
+}
+
+leo(80) #run the function
