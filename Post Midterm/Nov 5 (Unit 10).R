b1969b9f5c6cd4fd55488467c9e694285ed797ba
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-05 15:18:23 -0500

    Update Nov 5 (Unit 10).R

--- Post Midterm/Nov 5 (Unit 10).R
+++ Post Midterm/Nov 5 (Unit 10).R
@@ -1,12 +1,13 @@
 #Exercise 2
 x = 40
+
 if (x>=60){
   print("Pass")
 }else{
   print("Fail")
 }
-#Self-Defined Function
 
+#Self-Defined Function
 leo = function(x){
   if (x>=60){
     print("Pass")
@@ -16,3 +17,28 @@ leo = function(x){
 }
 
 leo(80) #run the function
+
+
+#Exercise 3 
+
+x=3
+
+if (x == 1) {
+  print("Mon")
+} else if (x == 2) {
+  print("Tue")
+} else if (x == 3) {
+  print("Wed")
+} else if (x == 4) {
+  print("Thu")
+} else if (x == 5) {
+  print("Fri")
+} else if (x == 6) {
+  print("Sat")
+} else if (x == 7) {
+  print("Sun")
+} else {
+  print("Enter an integer between 1 and 7")
+}
+
+
