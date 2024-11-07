e69d9ede5594562df82a64b4ce4cfc0481f9fad3
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-07 15:35:52 -0500

    Update Nov 7 (Unit 11) Loops.R

--- Post Midterm/Nov 7 (Unit 11) Loops.R
+++ Post Midterm/Nov 7 (Unit 11) Loops.R
@@ -1,1 +1,21 @@
+b = 0
+for(i in 1:3){
+  b=b+i
+  print(b)
+}
 
+b
+
+#Exercise 1
+
+# Create the data frame
+name <- c("Amy","Bob","Cindy","David","Eddie")
+grade <- c(88,98,92,82,85)
+program <- c(F,T,T,F,F)
+students <- data.frame(name,grade,program)
+
+
+
+for (i in names(students)) {
+  print(class(students[[i]]))
+}
