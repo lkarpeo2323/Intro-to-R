8a666f2cc2fa24c2051278216fe8f00c54b244b1
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-10 11:30:28 -0500

    Update HW5.R

--- HW/HW5.R
+++ HW/HW5.R
@@ -6,3 +6,16 @@ data = read.csv("gap.csv")
 #1B
 names(data)
 summary(data)
+
+
+#2A
+data$factor = factor(
+  ifelse(data$femaleemployrate <= 50, "Low", "High"),
+  levels = c("Low", "High")
+)
+
+# 2B: Put the frequency counts of "Low" and "High" in the "factor" variable
+table(data$factor)
+
+
+
