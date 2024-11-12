a90e8828d3a23963734aa4127fe4df8841dcd306
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-12 15:23:23 -0500

    Create Nov 12 (Unit 11).R

---
+++ Post Midterm/Nov 12 (Unit 11).R
@@ -0,0 +1,20 @@
+name <- c("Amy","Bob","Cindy","David","Eddie")
+grade <- c(88,98,92,82,85)
+program <- c(F,T,T,F,F)
+students <- data.frame(name,grade,program)
+
+
+names(students)[1] #Retrieve the name of the first column
+class(students[[1]]) #Retrieve the class of a column
+
+
+#Exercise 2
+
+i = 1
+while (i <= ncol(students)){
+  print(paste(
+    names(students)[i], 
+              "is", 
+    class(students[[i]])))
+  i = i+1 #Must have increment AFTER the print
+}
