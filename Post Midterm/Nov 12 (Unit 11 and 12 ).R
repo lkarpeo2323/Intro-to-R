b01415e26b308c6a5e55f3eb3265aadc6439a768
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-12 15:52:05 -0500

    Update Nov 12 (Unit 11 and 12 ).R

--- Post Midterm/Nov 12 (Unit 11 and 12 ).R
+++ Post Midterm/Nov 12 (Unit 11 and 12 ).R
@@ -8,7 +8,7 @@ names(students)[1] #Retrieve the name of the first column
 class(students[[1]]) #Retrieve the class of a column
 
 
-#Exercise 2
+#Exercise 2 'While' Loop
 
 i = 1
 while (i <= ncol(students)){
@@ -17,4 +17,23 @@ while (i <= ncol(students)){
               "is", 
     class(students[[i]])))
   i = i+1 #Must have increment AFTER the print
+}
+
+
+
+#Exercise 3: 'Repeat' Method
+
+i = 1
+repeat {
+  if (i > ncol(students)) {
+    break  
+  }
+  
+  print(paste(
+    names(students)[i], 
+    "is", 
+    class(students[[i]])
+  ))
+  
+  i <- i + 1
 }
