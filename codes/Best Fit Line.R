575fe3e1338a6067522c531a7393eb6af5f47379
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-19 15:31:32 -0500

    Update and rename Best Fit Line.R to Data Visualization.R

--- codes/Best Fit Line.R
+++
@@ -1,9 +0,0 @@
-data(mtcars)
-
-#Scatter Plot
-plot(mtcars$hp, mtcars$mpg) # hp= x-axis, mpg=y-axis
-
-fit = lm(mpg~hp, data=mtcars) #mpg is y-axis, hp is x-axis
-
-#Best Fit Line
-abline(fit,lty=5)
---
+++ codes/Data Visualization.R
@@ -0,0 +1,31 @@
+data(mtcars)
+
+#Best Fit Line
+
+
+#Scatter Plot
+plot(mtcars$hp, mtcars$mpg) # hp= x-axis, mpg=y-axis
+
+fit = lm(mpg~hp, data=mtcars) #mpg is y-axis, hp is x-axis
+
+abline(fit,lty=5) # Plot the best fit line
+
+#------------------------------
+
+#Pie Chart
+scoring = c(exam=50,hw=30,exercise=20)
+pie(scoring)
+
+#---------------------------------------------
+
+#Bar Plot
+
+barplot(scoring)
+#--------------
+
+#Barplot labeled by name
+name <- c("Amy","Bob","Cindy","David","Eddie")
+grade <- c(88,98,92,82,85)
+students <- data.frame(name,grade)
+
+barplot(grade~name,data=students,xlab="Student Name",ylab="Grades")
