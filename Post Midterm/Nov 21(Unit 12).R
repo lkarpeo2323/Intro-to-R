cafc8f5a09e74f6ae85a76c17ecf319fe31a87ed
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-21 15:14:48 -0500

    Update Nov 21(Unit 12).R

--- Post Midterm/Nov 21(Unit 12).R
+++ Post Midterm/Nov 21(Unit 12).R
@@ -1,1 +1,17 @@
+#Exercise 6
 
+data(mtcars)
+
+var = names(mtcars)
+var
+
+pdf("ScatterPlots.pdf")
+par(mfrow = c(3,2))
+
+for (i in 3:7){
+  plot(mtcars[[1]]~mtcars[[i]], 
+       main = paste("Scatter Plot of MPG v.s.", var[3]))
+  
+}
+
+dev.off()
