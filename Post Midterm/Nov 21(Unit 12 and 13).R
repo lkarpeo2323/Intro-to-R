eb0b479d67d16c2abcb7dda02da8c4923f259bdd
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-26 15:20:34 -0500

    Update Nov 21(Unit 12 and 13).R

--- Post Midterm/Nov 21(Unit 12 and 13).R
+++ Post Midterm/Nov 21(Unit 12 and 13).R
@@ -15,3 +15,23 @@ for (i in 3:7){
 }
 
 dev.off()
+
+#---------------------------------------------------
+
+
+
+# Unit 13 Exercise 3
+
+#Test the effect of Gender on Internet use
+data = read.csv("Telemedicine_extract.csv")
+names(data)
+
+chisq.test(data$Gender, data$InternetUse)
+
+#p-value =  0.8996
+#we cannot reject null hypothesis: 
+#we do not have evidence that internet use is effected by gender
+
+
+a = table(data$Gender, data$InternetUse)
+barplot(a, beside=T, legend=T)
