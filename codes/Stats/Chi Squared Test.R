2d6347eaff9e47d13fd57696051566d84313b61e
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-26 15:00:09 -0500

    Update Chi Squared Test.R

--- codes/Stats/Chi Squared Test.R
+++ codes/Stats/Chi Squared Test.R
@@ -1,8 +1,6 @@
 data = read.csv("Telemedicine_extract.csv")
 data
 
-#Chi Squared test
-
 #Gender and Internet use
 
 names(data)
@@ -12,3 +10,6 @@ chisq.test(data$Gender, data$InternetUse)
 #p-value =  0.8996
 #we cannot reject null hypothes: 
 #we do not have evidence that internet use is effected by gender
+
+
+#Best to use a bar plot
