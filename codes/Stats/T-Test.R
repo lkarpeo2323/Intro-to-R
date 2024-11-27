96b3db1b169f008d3ead78156481695d0b7b3500
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-27 17:01:33 -0500

    Create T-Test.R

---
+++ codes/Stats/T-Test.R
@@ -0,0 +1,11 @@
+#Figure Out if weights are different
+
+college = read.csv("t-Test.csv")
+
+t.test(college$Weight~college$University, data=college)
+
+
+
+
+boxplot(college$Weight~college$University, data=college)
+        
