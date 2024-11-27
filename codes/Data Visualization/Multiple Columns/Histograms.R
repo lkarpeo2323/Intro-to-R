c3a4cdccbf04e722c89882a000e6644c68664bf9
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-27 17:40:41 -0500

    Create Histograms.R

---
+++ codes/Data Visualization/Multiple Columns/Histograms.R
@@ -0,0 +1,23 @@
+var = names(mtcars)
+var
+
+#Send to PDF
+
+pdf("ScatterPlots.pdf")
+
+
+par(mfrow = c(3,2))
+
+for (i in 1:7) {
+  if (i==2){ #Exclude column 2
+    i=i+1
+  } else{
+  hist(mtcars[[var[i]]], 
+       main = paste("Histogram of", var[i]), 
+       xlab = var[i], 
+       ylab = "Frequency", 
+       )
+  }
+}
+
+dev.off()
