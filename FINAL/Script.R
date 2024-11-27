5d3d6e60e5d2810b9eea7f8a0acadbf3d4b19019
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-27 13:46:02 -0500

    Update Script.R

--- FINAL/Script.R
+++ FINAL/Script.R
@@ -3,7 +3,7 @@ data = read.csv("US  E-commerce records 2020_YC.csv")
 
 #Find the effect of covid on sales. 
 
-#Differnce is avg sales per day before and after 3/13/2020
+#Differnce is avg sales per day before and after 3/15/2020
 
 chisq.test(data$State, data$Category)
 
