27d34aa108327bc21ee23804a26509ba79ae0c47
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-27 13:46:52 -0500

    Update Script.R

--- FINAL/Script.R
+++ FINAL/Script.R
@@ -5,6 +5,7 @@ data = read.csv("US  E-commerce records 2020_YC.csv")
 
 #Differnce is avg sales per day before and after 3/15/2020
 
+#Run PAIRED T-Test
 chisq.test(data$State, data$Category)
 
 data
