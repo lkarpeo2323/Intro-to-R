0486eb73dc9f8edcbdb517945e98d14af57ab395
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-02 08:41:41 -0500

    Update HW7.R

--- HW/HW7.R
+++ HW/HW7.R
@@ -2,8 +2,31 @@
 
 visit = read.csv("Telemedicine_extract.csv")
 
+#2
 
+#a
 
+#------------------------------
+#b
+
+t.test(visit$WaitMinutes~visit$Gender, data=visit)
+
+#we do not have evidence that suggests wait minutes is effected by gender
+
+#---------------------------------
+#c
+
+summary(lm(visit$WaitMinutes~visit$Gender,data=visit))
+
+#------------------
+#d
+
+boxplot(visit$WaitMinutes~visit$Gender, data=visit, 
+        main = "Boxplot of Wait Minutes by Gender",
+        xlab="Gender", 
+        ylab="Wait Minutes")
+
+dev.off()
 
 
 
