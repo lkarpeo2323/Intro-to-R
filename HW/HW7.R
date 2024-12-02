3bf2889ae8d9942b39ff95f8266f392869de27a1
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-02 08:48:13 -0500

    Update HW7.R

--- HW/HW7.R
+++ HW/HW7.R
@@ -1,8 +1,6 @@
 #1 
-
 visit = read.csv("Telemedicine_extract.csv")
-
-#2
+#2--------------------------------------------
 
 #a
 
@@ -11,20 +9,22 @@ visit = read.csv("Telemedicine_extract.csv")
 
 t.test(visit$WaitMinutes~visit$Gender, data=visit)
 
+#p-value = 0.5455
 #we do not have evidence that suggests wait minutes is effected by gender
 
 #---------------------------------
 #c
-
 summary(lm(visit$WaitMinutes~visit$Gender,data=visit))
+
+#p-value: 0.5617
+# No evidence that suggests gender has a strong relationship with waiting time
 
 #------------------
 #d
-
 boxplot(visit$WaitMinutes~visit$Gender, data=visit, 
-        main = "Boxplot of Wait Minutes by Gender",
+        main = "Boxplot of Wait Time by Gender",
         xlab="Gender", 
-        ylab="Wait Minutes")
+        ylab="Wait Time (Minutes)")
 
 dev.off()
 
