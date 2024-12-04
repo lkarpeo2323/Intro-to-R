41f127ed09143e69af9de4a092ac2742cc211a23
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 18:27:44 -0500

    Update Order Count by City.R

--- FINAL/Order Count by City.R
+++ FINAL/Order Count by City.R
@@ -1,2 +1,28 @@
 data = read.csv("US  E-commerce records 2020_YC.csv")
 library(psych)
+
+factor(data$Order.ID)
+cities = aggregate(data$Order.ID ~ City, data = data, length)
+colnames(cities) = c("City", "Number of Orders")
+
+##----------- Histogram --------------##
+#Show how the number of orders is distributed across cities so we  can make barplot
+
+hist(cities$`Number of Orders`, 
+     breaks=(nrow(cities)/2), 
+     main = "Distribution of Order Counts Across Cities", 
+     xlab = "Order Count", 
+     ylab = "Frequency")
+
+dev.off()
+
+##------------- Barplot --------------##
+most = cities[cities$`Number of Orders`>25, ]
+
+barplot(`Number of Orders`~City,data=most,
+        xlab="City Name",
+        ylab="Total Orders", 
+        las=2)
+
+
+dev.off()
