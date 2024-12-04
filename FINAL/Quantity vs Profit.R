3529eaf0d8f0299808a2a46d13ca0cd1fe8c2e8d
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 17:21:50 -0500

    Update Quantity vs Profit.R

--- FINAL/Quantity vs Profit.R
+++ FINAL/Quantity vs Profit.R
@@ -18,3 +18,22 @@ boxplot(loss$Quantity~factor(loss$Sub.Category),
 
 
 List the Mean of the Quantity of positive Profit and negative Profit
+
+
+
+gain = data[data$Sub.Category=='Copiers'| 
+              data$Sub.Category=='Accessories'| 
+              data$Sub.Category=='Phones'| 
+              data$Sub.Category=='Paper', 
+              data$Sub.Category=='Appliances', 
+              data$Sub.Category=='Binders', 
+              data$Sub.Category=='Chairs', 
+              data$Sub.Category=='Storage', 
+              data$Sub.Category=='Furnishings', 
+              data$Sub.Category=='Art',
+              data$Sub.Category=='Labels',
+              data$Sub.Category=='Envelopes',
+              data$Sub.Category=='Fasteners'
+            
+]
+
