2b9261493a44b42e1e2606b3ff5a007ef1ec07ea
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 16:56:02 -0500

    Create Quantity vs Profit.R

---
+++ FINAL/Quantity vs Profit.R
@@ -0,0 +1,17 @@
+#################### Quantity vs Profit(Loss) #####################
+
+loss = data[data$Sub.Category=='Tables'| 
+              data$Sub.Category=='Supplies'| 
+              data$Sub.Category=='Machines'| 
+              data$Sub.Category=='Bookcases', 
+            ]
+
+summary(aov(Quantity ~ Sub.Category, data = loss))
+
+
+boxplot(loss$Quantity~factor(loss$Sub.Category),
+        main = "Quantity vs Sub-Category",
+        xlab=" ",
+        ylab = "Quantity", 
+        las=2,
+)
