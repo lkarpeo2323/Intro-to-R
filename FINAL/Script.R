d73557fe0af72a3a7c342292a34799f397b62a8c
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 16:55:24 -0500

    Update Script.R

--- FINAL/Script.R
+++ FINAL/Script.R
@@ -74,9 +74,23 @@ pie(negative$Profit,
 
 dev.off()
 
+#################### Quantity vs Profit(Loss) #####################
+
+loss = data[data$Sub.Category=='Tables'| 
+              data$Sub.Category=='Supplies'| 
+              data$Sub.Category=='Machines'| 
+              data$Sub.Category=='Bookcases', 
+            ]
+
+summary(aov(Quantity ~ Sub.Category, data = loss))
 
 
-
+boxplot(loss$Quantity~factor(loss$Sub.Category),
+        main = "Quantity vs Sub-Category",
+        xlab=" ",
+        ylab = "Quantity", 
+        las=2,
+)
 
 
 
