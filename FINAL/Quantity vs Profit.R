68107f34eae9cd6e7ba9c87422c1dcdc3d9a9d35
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 17:24:17 -0500

    Update Quantity vs Profit.R

--- FINAL/Quantity vs Profit.R
+++ FINAL/Quantity vs Profit.R
@@ -24,16 +24,19 @@ List the Mean of the Quantity of positive Profit and negative Profit
 gain = data[data$Sub.Category=='Copiers'| 
               data$Sub.Category=='Accessories'| 
               data$Sub.Category=='Phones'| 
-              data$Sub.Category=='Paper', 
-              data$Sub.Category=='Appliances', 
-              data$Sub.Category=='Binders', 
-              data$Sub.Category=='Chairs', 
-              data$Sub.Category=='Storage', 
-              data$Sub.Category=='Furnishings', 
-              data$Sub.Category=='Art',
-              data$Sub.Category=='Labels',
-              data$Sub.Category=='Envelopes',
+              data$Sub.Category=='Paper' |
+              data$Sub.Category=='Appliances'|
+              data$Sub.Category=='Binders'|
+              data$Sub.Category=='Chairs' |
+              data$Sub.Category=='Storage'|
+              data$Sub.Category=='Furnishings'| 
+              data$Sub.Category=='Art'|
+              data$Sub.Category=='Labels'|
+              data$Sub.Category=='Envelopes'|
               data$Sub.Category=='Fasteners'
             
 ]
 
+
+
+mean(gain$Sub.Category)
