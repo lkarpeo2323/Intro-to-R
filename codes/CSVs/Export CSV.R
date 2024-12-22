54dd83d578233b88af595083840a0014629dcb5a
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-21 22:49:00 -0500

    Delete codes directory

--- codes/CSVs/Export CSV.R
+++
@@ -1,8 +0,0 @@
-#Exercise 2
-
-Student = c("Apple","Ben","Carol","Debby","Eddie")
-Grade = c(80,75,92,86,78)
-Class = data.frame(Student,Grade)
-Class
-
-write.csv(Class,"output.csv",row.names=FALSE)
--- codes/CSVs/Read CSV.R
+++
@@ -1,5 +0,0 @@
-#Read CSV
-
-text_file = read.table("Amazon.txt",header=TRUE)
-csv_file = read.csv("Amazon.csv",header=TRUE)
-print(csv_file)
